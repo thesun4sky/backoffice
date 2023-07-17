8865dafab90a3153ed360fbf4244632322274d42
jiinse0 <jee_ji@naver.com>
2023-07-17 12:31:07 +0900

    init commit

---
+++ src/main/java/com/sparta/backoffice/BackofficeApplication.java
@@ -0,0 +1,15 @@
+package com.sparta.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BackofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackofficeApplication.class, args);
+    }
+
+}
