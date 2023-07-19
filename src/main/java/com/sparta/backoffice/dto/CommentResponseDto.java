e2527767035601bccfb27ba4a0820e7278243a43
azuressu <martina0130@naver.com>
2023-07-19 11:48:24 +0900

    게시글 반환 내용과 댓글 반환 내용 수정

--- src/main/java/com/sparta/backoffice/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/backoffice/dto/CommentResponseDto.java
@@ -12,17 +12,27 @@ public class CommentResponseDto {
 
     private Long id;                   // 댓글 ID
     private String username;           // 댓글 작성자
+    private String nickname;
     private String commentcontents;    // 댓글 내용
     private LocalDateTime createdAt;   // 댓글 작성일
     private LocalDateTime modifiedAt;  // 댓글 수정일
     private Integer commentlikeCount;  // 댓글 좋아요 수
+    private Boolean liked;
+    private String like;
 
     public CommentResponseDto(Comment comment) {
         this.id = comment.getId();
         this.username = comment.getUser().getUsername();
+        this.nickname = comment.getUser().getNickname();
         this.commentcontents = comment.getCommentcontents();
         this.createdAt = comment.getCreatedAt();
         this.modifiedAt = comment.getModifiedAt();
         this.commentlikeCount = comment.getCommentlikeCount();
     }
+
+    public void setLike(Boolean b) {
+        this.liked = b;
+    }
+    public void setStringLike(String s) {this.like = s;}
+
 }
--- src/main/java/com/sparta/backoffice/service/CommentService.java
+++ src/main/java/com/sparta/backoffice/service/CommentService.java
@@ -3,8 +3,10 @@ package com.sparta.backoffice.service;
 import com.sparta.backoffice.dto.CommentRequestDto;
 import com.sparta.backoffice.dto.CommentResponseDto;
 import com.sparta.backoffice.entity.Comment;
+import com.sparta.backoffice.entity.CommentLike;
 import com.sparta.backoffice.entity.Post;
 import com.sparta.backoffice.entity.User;
+import com.sparta.backoffice.repository.CommentLikeRepository;
 import com.sparta.backoffice.repository.CommentRepository;
 import com.sparta.backoffice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 import static com.sparta.backoffice.entity.UserRoleEnum.ADMIN;
 
@@ -22,6 +27,40 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
+    private final CommentLikeRepository commentLikeRepository;
+
+    // 댓글 반환 리스트 수정 .. (해당 댓글 반환 타입마다 boolean 타입의 속성을 지정해줌)
+    public List<CommentResponseDto> commentlikefind(Long postid, User user, List<CommentResponseDto> list) {
+        Post post = findPost(postid);
+
+        // comment like 에 있는 post에 대한 commentlike 리스트를 만든다 (해당 사용자와, post에 대한 리스트만 갖고옴)
+        // 해당 게시글에 있는 댓글들 중, 사용자가 좋아요를 누른 댓글들의 좋아요 리스트만 갖고오게 됨
+        List<CommentLike> commentlikeList = commentLikeRepository.findAllByUserAndPost(user, post).stream().toList();
+        // 그 둘을 비교해서 responstdto에 boolean을 setting 해주면 되는데
+
+        for (CommentResponseDto c: list) {
+            // 여기서 이제 하나씩 비교.. 를 해야되는데 .. 이중 for문 돌겠네
+            for (CommentLike cc: commentlikeList) {
+                // commentlike에 있는 댓글의 ld와, commentresponsedto에 있는 댓글의 id가 일치하면
+                if (cc.getComment().getId() == c.getId()) {
+                    if (cc.getLike()) {
+                        log.info("좋아요 되어있는 commentid: "+ c.getId());
+                        c.setLike(true);
+                        c.setStringLike("true");
+                    } else {
+                        log.info("좋아요 안되어있는 commentid: "+ c.getId());
+                        c.setLike(false);
+                    }
+                } else {
+                    c.setLike(false);
+                }
+            }
+        }
+
+        // 뭘 return 해줘야하나 ...
+        return list;
+    }
+
 
     @Transactional
     public CommentResponseDto createComment(Long postid, CommentRequestDto commentRequestDto, User user) {
@@ -87,7 +126,15 @@ public class CommentService {
 
     }
 
-    private Comment findComment(Long commentid) {
-        return commentRepository.findById(commentid).orElseThrow();
+    private Post findPost(Long id) {
+        return postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+    }
+
+    private Comment findComment(Long id) {
+        return commentRepository.findById(id).orElseThrow();
+    }
+
+    private Optional<CommentLike> findCommentLike(Comment comment, User user) {
+        return commentLikeRepository.findByCommentAndUser(comment, user);
     }
 }
